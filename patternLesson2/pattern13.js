1dc6be19a2840f8eb4683c288efcfed1b41615f8
amal jose <amal.jose@appsintegra.com>
2019-11-06 07:13:51 +0400

    Pattern 13

---
+++ patternLesson2/pattern13.js
@@ -0,0 +1,20 @@
+
+const printPattern=(count)=>{
+    let rowOutput
+    for(i=0;i<count;i++) {
+        rowOutput=""  
+            for(k=0;k<count-i;k++) {
+               rowOutput+="*"
+            }
+        console.log(rowOutput)
+    }
+    for(i=2;i<=count;i++) {
+        rowOutput=""  
+            for(k=0;k<i;k++) {
+               rowOutput+="*"
+            }
+        console.log(rowOutput)
+    }
+}
+printPattern(5)
+
