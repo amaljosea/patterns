acd3e751f0c3ad58e43671f1e5817a64cdfce0f2
amal jose <amal.jose@appsintegra.com>
2019-11-07 06:53:48 +0400

    Maximum number of solutions adding

--- patternLesson1/pattern1.js
+++ patternLesson1/pattern1.js
@@ -1,23 +1,42 @@
-
-
-const printPattern =(count)=>{
-    let complexity =0
-    let lineString=""
-    for(i=0;i<count; i++) {
-        complexity++
-        for(j=0;j<count-i; j++) {
-            complexity++
-            lineString+=" "
-        }
-        for(k=0;k<i+1;k++) {
-            complexity++
-            lineString+=(i+1+" ")
-        }
-        console.log(lineString)
-        lineString=""
+const printPattern1 = count => {
+  console.log("\n===printPattern1===\n");
+  let complexity = 0;
+  let lineString = "";
+  for (i = 0; i < count; i++) {
+    complexity++;
+    for (j = 0; j < count - i; j++) {
+      complexity++;
+      lineString += " ";
     }
-    console.log("\ncomplexity:", complexity)
-}
+    for (k = 0; k < i + 1; k++) {
+      complexity++;
+      lineString += i + 1 + " ";
+    }
+    console.log(lineString);
+    lineString = "";
+  }
+  console.log("\ncomplexity:", complexity);
+};
 
-printPattern(9)
+const printPattern2 = count => {
+  console.log("\n===printPattern2===\n");
+  let complexity = 0;
+  let lineString = "";
+  for (i = 0; i < count; i++) {
+    complexity++;
+    for (j = 0; j < count - i; j++) {
+      complexity++;
+      lineString += " ";
+    }
+    for (k = 0; k < i + 1; k++) {
+      complexity++;
+      lineString += i + 1 + " ";
+    }
+    console.log(lineString);
+    lineString = "";
+  }
+  console.log("\ncomplexity:", complexity);
+};
 
+printPattern1(9);
+printPattern2(9);
