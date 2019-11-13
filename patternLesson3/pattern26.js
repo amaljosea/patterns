0551e29f1803ae1701780caeb050b61fd764d612
amal jose <amal.jose@appsintegra.com>
2019-11-13 07:49:18 +0400

    Adding method 2 for pattern 26

--- patternLesson3/pattern26.js
+++ patternLesson3/pattern26.js
@@ -1,5 +1,5 @@
 
-const printPattern=(rowCount)=>{
+const printPattern1=(rowCount)=>{
     let rowOutput
     totalIterationCount=2*rowCount-1
     for(i=0;i<totalIterationCount;i++) {
@@ -31,4 +31,32 @@ const printPattern=(rowCount)=>{
     }
 }
 
-printPattern(6)
+const printPattern2=(rowCount)=>{
+    let rowOutput
+    totalIterationCount=2*rowCount-1
+    for(i=0;i<totalIterationCount;i++) {
+        rowOutput=""    
+        if(i<rowCount) {
+            for(j=0;j<rowCount;j++) {
+                if(j<=i) rowOutput+="*"
+                 else rowOutput+=" " 
+            }
+            for(k=0;k<=rowCount;k++) {
+                if(k<rowCount-i) rowOutput+=" "  
+                    else rowOutput+="*" 
+            }
+        } else {
+            for(l=0;l<rowCount;l++) {
+                if(l<=totalIterationCount-1-i)rowOutput+="*"
+                else rowOutput+=" " 
+            } 
+            for(k=0;k<=rowCount;k++) {
+                if(k<=i-totalIterationCount/2+1) rowOutput+=" "
+                 else rowOutput+="*" 
+            }
+        }   
+        console.log(rowOutput)
+    }
+}
+printPattern1(6)
+printPattern2(6)
