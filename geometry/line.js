d10ead90bc41cce63e55b759693fb145e6b5b43a
amal jose <amal.jose@appsintegra.com>
2019-11-23 08:05:01 +0400

    Adding line and heading

--- geometry/line.js
+++ geometry/line.js
@@ -1,9 +1,8 @@
-const print = () =>{
+const printLine = () =>{
     let i, strOur=""
-    for(i=0;i<50;i++) {
-        strOur+="*"
+    for(i=0;i<window.outerWidth/5.5 ;i++) {
+        strOur+="-"
     }
     console.log(strOur)
 }
-console.log("strOur")
-print()
+printLine()
