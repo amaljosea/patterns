8111923a8c33d36039743e299584276dc1189d37
amal jose <amal.jose@appsintegra.com>
2019-12-11 08:06:58 +0400

    Adding plugins, blog page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,14 @@ import Projects from '../components/projects'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Intro/>
+    <div>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+  </div>
+    {/* <Intro/> */}
     <Tech/>
     <Projects/>
     <Link to="/page-2/">Works</Link>
