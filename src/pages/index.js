f5353ef299036f5a659b485460f99e3718894572
amal jose <amal.jose@appsintegra.com>
2019-12-23 19:23:57 +0400

    Adding * in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,17 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div>
+    <div style={{width:"250px"}}>
+    * * * * * * * * * * * * * * * * * * * *
+    * * * * * * * * * * * * * * * * * * * *
+    * * * * * * * * * * * * * * * * * * * *
+    * * * * * * * * * * * * * * * * * * * *
+    * * * * * * * * * * * * * * * * * * * *
+    * * * * * * * * * * * * * * * * * * * *
+    * * * * * * * * * * * * * * * * * * * *
+    * * * * * * * * * * * * * * * * * * * *
+    * * * * * * * * * * *
+    </div>
     <h2>Pattern programming questions.</h2>
     <p>
       It is fun to learn programming by printing patterns. This project contains collection of different patterns and some solutions for printing them using javascript.
@@ -15,6 +26,8 @@ const IndexPage = () => (
     <p>
       For contributing, clone the repo at: <a href="https://github.com/howareyouami/patterns">github</a>
     </p>
+
+
   </div>
   </Layout>
 )
