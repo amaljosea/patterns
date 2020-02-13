02e705ca387108babeb587f004cbffac6be3ee67
amal jose <amal.jose@appsintegra.com>
2020-02-13 06:36:24 +0400

    Live code output

--- src/components/Console/index.js
+++ src/components/Console/index.js
@@ -1,31 +1,47 @@
 import React from "react"
 
-const Console = ({solution}) => (
-    <>
-        <h3>Output</h3>
-        <iframe
-            style={{
-                width: "100%",
-                height: "40vh",
-                overflow: "scroll",
-                border: 0,
-                backgroundColor: "whitesmoke"
-            }}
-            srcDoc={`
+const Console = ({ solution }) => (
+  <>
+    <h3>Output</h3>
+    <iframe
+      style={{
+        width: "100%",
+        height: "40vh",
+        overflow: "scroll",
+        border: 0,
+        backgroundColor: "whitesmoke"
+      }}
+      srcDoc={`
   <html>
    <script> 
-   console.log = (...arguments) => {
-    arguments.forEach((item)=>{
-      document.getElementById("output").innerHTML +=
-      item.toString().replace(/ /g, "&nbsp&nbsp") + "<br>";
-    })
-  };
+   try {
+    console.log = (...arguments) => {
+      arguments.forEach((item)=>{
+        document.getElementById("output").innerHTML +=
+        item.toString().replace(/ /g, "&nbsp&nbsp") + "<br>";
+      })
+    };
+    document.showError = (...arguments) => {
+      arguments.forEach((item)=>{
+        document.getElementById("error").innerHTML +=
+        item.toString().replace(/ /g, "&nbsp&nbsp") + "<br>";
+      })
+    };
+   } catch (e){
+    document.showError(e)
+   }
    </script>
     <body>
-    <div id="output">
+    <div id="error" style="color:red"></div>
+    <div id="output"></div>
     </body>
     <script> 
-    ${solution}
+    try{
+      ${solution}
+    } 
+    catch(e){
+      document.showError(e)
+    }
     </script>
     <style>
     #output{
@@ -34,8 +50,8 @@ const Console = ({solution}) => (
     </style>
     </html>
    `}
-        />
-    </>
+    />
+  </>
 );
 
 
--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -1,11 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import Console from '../Console'
 
 const EditorWrapper = styled.div`
 `
 
-const TextArea = styled.textarea`
+const UserCodeArea = styled.textarea`
   width: 100%;
   height: 40vh;
 `
@@ -21,18 +21,32 @@ const ButtonWrapper = styled.div`
   justify-content: flex-end;
 `
 
-const Editor = ({solution}) => (
+const Editor = ({solution}) => {
+  const [argument,setArugment]=useState('')
+  const [userCode,setUserCode]=useState('')
+
+  const onArgumentChange=(e)=>{
+    setArugment(e.target.value)
+  }
+
+  const onUserCodeChange=(e)=>{
+    setUserCode(e.target.value)
+  }
+
+  return (
     <EditorWrapper>
-        <TextArea/>
-        <ArgumentInput placeholder="Argument goes here..."/>
+        <UserCodeArea value={userCode} onChange={onUserCodeChange}/>
+        <ArgumentInput placeholder="Argument goes here..." onChange={onArgumentChange} value={argument}/>
         <ButtonWrapper>
             <button>Run</button>
             <button>Test</button>
             <button>View solution</button>
         </ButtonWrapper>
-        <Console solution={solution}/>
+        <Console
+        //  solution={solution} 
+         solution={userCode}/>
     </EditorWrapper>
-)
+)}
 
 
 export default Editor
