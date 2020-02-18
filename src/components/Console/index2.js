43f709f81999d17f4448a0601fe6d6ee0b7d8d3e
amal jose <amal.jose@appsintegra.com>
2020-02-18 19:47:00 +0400

    Sidebar hiding problem fixed

--- src/components/Console/index2.js
+++ src/components/Console/index2.js
@@ -3,13 +3,16 @@ import styled from "styled-components"
 
 const Console = ({ output, isError }) => {
   const Pre = styled.pre`
+  height: 40vh;
+  overflow: scroll;
+  width: 100%;
   color:${isError ? "red" : ''};
 `
   return (
-    <>
+    <div>
       <h3>Output</h3>
       <Pre>{output}</Pre>
-    </>
+    </div>
   )
 };
 
